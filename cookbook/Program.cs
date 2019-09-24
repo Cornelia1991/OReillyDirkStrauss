6e32ca9fe306ed610f19f86b80d34096b8ae5e34
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-23 20:21:20 -0400

    Tuples and Extension Methods

---
+++ cookbook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+
+namespace cookbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 17, 46, 39, 62, 81, 79, 52, 24, 65 };
+            chapter1 ch1 = new chapter1();
+            int threshold = 51;
+            var (average, studentCount, belowAverage) = ch1.GetAverageAndCount(scores, threshold);
+            WriteLine($"Average was {Round(average, 2)} across {studentCount} students. {(average < threshold ?"Class score below average." : "Class score above average" )}");
+            ReadLine();
+        }
+    }
+}
---
+++ cookbook/chapter1.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using static System.Console;
+using static System.Math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//explanantion of tuples
+//need to calulate average score for variable number of students
+//want our method to return of students in class
+namespace cookbook
+{
+    public class chapter1
+    {
+        public (double average, int studentGrade, bool belowAverage) GetAverageAndCount(int[] scores, int threshold) //new
+        {
+            var returnTuple = (ave: 0D, sCount: 0, subAve: true); //tuple literal
+
+            returnTuple = ((double)scores.Sum() / scores.Count(), scores.Count(), returnTuple.ave.CheckIfBelowAverage(threshold));
+
+            return returnTuple;
+        }
+    }
+
+    public static class ExtentionMethods //new
+    {
+        public static bool CheckIfBelowAverage(this double classAverage, int threshold)
+        {
+            if (classAverage < threshold)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+}
